13b4569f2a923b71662087c3d15e3560ddf6046f
Alex Miller <apmiller108@yahoo.com>
2018-04-27 23:57:17 -0400

    Implement Agent to keep track of items and quantities

---
+++ lib/bucket.ex
@@ -0,0 +1,27 @@
+defmodule KV.Bucket do
+  use Agent
+
+  def start_link(_options) do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  def get(bucket, key) do
+    Agent.get(bucket, fn state -> Map.get(state, key) end)
+  end
+
+  def put(bucket, key, value) do
+    Agent.update(
+      bucket,
+      fn(state) -> Map.put(state, key, value) end
+    )
+  end
+
+  @doc """
+  Deletes `key` from `bucket`.
+
+  Returns the current value of `key`, if `key` exists.
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
+  end
+end
---
+++ test/kv/bucket_test.exs
@@ -0,0 +1,24 @@
+defmodule KV.BucketTest do
+  use ExUnit.Case, async: true
+
+  # Setup callback runs before the test in the same process
+  setup do
+    {:ok, bucket} = KV.Bucket.start_link([])
+    %{bucket: bucket}
+  end
+
+  # Pattern match on bucket from the callback result to use it in the test
+  test "stores values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.get(bucket, "milk") == 3
+  end
+
+  test "deletes values by key", %{bucket: bucket} do
+    assert KV.Bucket.delete(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.delete(bucket, "milk") == 3
+  end
+end
